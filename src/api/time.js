0995e96a095225585ee139e00266e20787517cd2
John Angel <usw4763@gmail.com>
2020-12-30 15:49:24 -0500

    Adding utility to get time information for a given location coordinates

---
+++ src/api/time.js
@@ -0,0 +1,22 @@
+import Axios from 'axios'
+
+const axios = Axios.create({ baseURL: 'http://api.timezonedb.com/v2.1/get-time-zone' })
+
+const apiKey = process.env.TIMEZONE_API_KEY
+
+/**
+ * Retrieves time information for the given coordiantes
+ * @param {{
+ *  lat: number,
+ *  lon: number
+ * }} args
+ */
+export const getTimeDetails = async ({ lat, lon }) => {
+  try {
+    const response = await axios.get(`?key=${apiKey}&by=position&lat=${lat}&lng=${lon}`)
+    return response.data
+  } catch (error) {
+    console.log(error)
+    return []
+  }
+}
