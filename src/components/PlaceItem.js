aba919560b61b5aabf80bb679396ef2d26975cad
John Angel <usw4763@gmail.com>
2020-12-30 15:50:42 -0500

    Adding call to fetch time/weather information for the selected place Adding dynamic main weather/time information when a place is selected

--- src/components/PlaceItem.js
+++ src/components/PlaceItem.js
@@ -1,12 +1,17 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
-import _ from 'lodash'
+import { getWeatherDetails } from '../api/weather'
+import { useSetRecoilState } from 'recoil'
+import { selectedPlaceState } from '../state/atoms'
+import { getTimeDetails } from '../api/time'
 
 /**
  * @typedef {object} Place
  * @prop {string} name
  * @prop {string} country
  * @prop {string} state
+ * @prop {string} lat
+ * @prop {string} lon
  */
 
 const Container = styled.div`
@@ -23,6 +28,7 @@ const CountryImage = styled.img`
 
 const PlaceDescription = styled.span`
   color: ${({ isBeingHovered }) => isBeingHovered ? 'white' : 'lightgray'};
+  font-weight: lighter;
 `
 
 /**
@@ -31,13 +37,15 @@ const PlaceDescription = styled.span`
  *  place: Place
  * }} props
  */
-export const PlaceItem = ({ place, onClick }) => {
+export const PlaceItem = ({ place }) => {
   const { country, name, state } = place
   const [isHovered, setIsHovered] = useState(false)
-  const handleClick = () => {
-    if (onClick && _.isFunction(onClick)) {
-      onClick(place)
-    }
+  const setSelectedPlace = useSetRecoilState(selectedPlaceState)
+  const handleClick = async () => {
+    const weatherPromise = getWeatherDetails({ lat: place.lat, lon: place.lon })
+    const timePromise = getTimeDetails({ lat: place.lat, lon: place.lon })
+    const [weather, time] = await Promise.all([weatherPromise, timePromise])
+    setSelectedPlace({ weather, time, details: place })
   }
   const countryFlagUrl = `https://www.countryflags.io/${country.toLowerCase()}/flat/24.png`
   return (
--- src/components/WeatherMainInformation.js
+++ src/components/WeatherMainInformation.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import styled from 'styled-components'
+import { useRecoilValue } from 'recoil'
+import _ from 'lodash'
+import { WiFog, WiDaySunny, WiSnow, WiRain, WiCloud } from 'react-icons/all'
+import { selectedPlaceState } from '../state/atoms'
 import { Row } from './Row'
 import { Column } from './Column'
 
 const Text = styled.span`
   color: white;
+  font-weight: lighter;
 `
 
 const Container = styled(Row)`
@@ -17,28 +22,74 @@ const Degrees = styled(Text)`
   font-size: 6em;
   line-height: 0.8;
   margin-right: 5px;
+  font-weight: normal;
 `
 
 const Place = styled(Text)`
   font-size: 2em;
+  font-weight: normal;
   margin-bottom: 5px;
   display: block;
 `
 
+const LocationDetailsContainer = styled(Column)`
+  padding-left: 5px;
+  padding-right: 5px;
+`
+
+const WeatherDescriptionContainer = styled(Column)`
+  padding-left: 20px;
+`
+
+/**
+ * Icon that describes the given weather type
+ * @param {{
+ *  type: string
+ * }} props
+ */
+const WeatherIcon = ({ type = '' }) => {
+  const props = { size: 40, color: 'white' }
+  switch (type) {
+    case 'Clear':
+      return <WiDaySunny {...props} />
+    case 'Clouds':
+      return <WiCloud {...props} />
+    case 'Rain':
+      return <WiRain {...props} />
+    case 'Snow':
+      return <WiSnow {...props} />
+    case 'Mist':
+      return <WiFog {...props} />
+    default:
+      return <React.Fragment />
+  }
+}
+
 export const WeatherMainInformation = () => {
+  const place = useRecoilValue(selectedPlaceState)
   return (
     <Container>
-      <Row style={{ alignItems: 'flex-end' }}>
-        <Column>
-          <Degrees>16°</Degrees>
-        </Column>
-        <Column>
-          <Place>London</Place>
-          <Row>
-            <Text>06:09 - Monday, 9 sep 2019</Text>
-          </Row>
-        </Column>
-      </Row>
+      {(!_.isEmpty(place) && !_.isEmpty(place.weather)) && (
+        <Row style={{ alignItems: 'flex-end' }}>
+          <Column>
+            <Degrees>{place.weather.main.temp}°</Degrees>
+          </Column>
+          <LocationDetailsContainer>
+            <Place>{place.details.name}, {place.details.country}</Place>
+            <Row>
+              <Text>06:09 - Monday, 9 sep 2019</Text>
+            </Row>
+          </LocationDetailsContainer>
+          <WeatherDescriptionContainer>
+            <Row style={{ marginBottom: 10 }}>
+              <WeatherIcon type={_.first(place.weather.weather).main} />
+            </Row>
+            <Row>
+              <Text>{_.first(place.weather.weather).main}</Text>
+            </Row>
+          </WeatherDescriptionContainer>
+        </Row>
+      )}
     </Container>
   )
 }
