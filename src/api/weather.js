ddb58133b08518cad14a139b3a0a8f28243bfa2f
John Angel <usw4763@gmail.com>
2020-12-30 15:49:48 -0500

    Adding utility to get weather information for a given location coordinates

--- src/api/weather.js
+++ src/api/weather.js
@@ -3,6 +3,8 @@ import Axios from 'axios'
 const axios = Axios.create({ baseURL: 'http://api.openweathermap.org/' })
 
 const GEO_API_PATH = '/geo/1.0/direct'
+const WEATHER_API_PATH = '/data/2.5/weather'
+const apiKey = process.env.REACT_APP_OPEN_WEATHER_API_KEY
 
 /**
  * Search places (cities) by the given value
@@ -10,11 +12,27 @@ const GEO_API_PATH = '/geo/1.0/direct'
  */
 export const searchPlaces = async (filterValue) => {
   try {
-    const apiKey = process.env.REACT_APP_OPEN_WEATHER_API_KEY
-    const response = await axios.get(`${GEO_API_PATH}?q=${filterValue}&limit=10&appid=${apiKey}`)
+    const response = await axios.get(`${GEO_API_PATH}?q=${filterValue}&limit=5&appid=${apiKey}`)
     return response.data
   } catch (error) {
     console.log(error)
     return []
+  }
+}
+
+/**
+ * Retrieves weather information for the given coordinates
+ * @param {{
+ *  lat: number,
+ *  lon: number
+ * }} args
+ */
+export const getWeatherDetails = async ({ lat, lon }) => {
+  try {
+    const response = await axios.get(`${WEATHER_API_PATH}?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`)
+    return response.data
+  } catch (error) {
+    console.log(error)
+    return {}
   }
 }
