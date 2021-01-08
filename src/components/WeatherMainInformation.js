ff7c92964f1959cf4e08f73d52077becdfa2b4b8
John Angel <usw4763@gmail.com>
2021-01-07 20:10:19 -0500

    Adding background to the main information

--- src/components/WeatherMainInformation.js
+++ src/components/WeatherMainInformation.js
@@ -10,14 +10,32 @@ import { Column } from './Column'
 import { WeatherIcon } from './WeatherIcon'
 import { selectedPlaceState } from '../state/atoms'
 
-const Container = styled(Row)`
+const Background = styled.main`
+  display: flex;
+  padding: 0.2em;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-radius: 6px;
+  ${breakpoint('tablet')`
+    padding: 3em;
+    background-color: #000000a3;
+  `}
+  ${breakpoint('desktop')`
+    align-items: flex-end;
+    padding: 3em;
+    flex-direction: row;
+    justify-content: flex-start;
+    background-color: #000000a3;
+  `}
+`
+
+const Container = styled(Column)`
   width: 100%;
-  padding: 1em;
   align-items: center;
   justify-content: center;
-  flex-direction: column;
   ${breakpoint('tablet')`
-    padding: 6em 1em;
+    padding: 1em 1em;
   `}
   ${breakpoint('desktop')`
     align-items: flex-end;
@@ -28,10 +46,15 @@ const Container = styled(Row)`
 `
 
 const Degrees = styled(Text)`
-  font-size: 6em;
+  font-size: 4em;
   line-height: 0.8;
   font-weight: normal;
   text-align: center;
+  margin-top: 10px;
+  ${breakpoint('tablet')`
+    margin-top: 0;
+    font-size: 6em;
+  `}
   ${breakpoint('desktop')`
     margin-right: 5px;
     text-align: left;
@@ -65,6 +88,7 @@ const LocationDetailsContainer = styled(Column)`
 
 const WeatherDescriptionContainer = styled(Column)`
   padding-top: 1em;
+  align-items: center;
   ${breakpoint('desktop')`
     padding-top: 0;
     padding-left: 20px;
@@ -79,19 +103,21 @@ export const WeatherMainInformation = () => {
   return (
     (hasAllValues && (
       <Container>
-        <Degrees>{current.weather.temperature}°</Degrees>
-        <LocationDetailsContainer>
-          <Place>{location.name}, {location.country}</Place>
-          <Text inline>{DateTime.fromFormat(time.formatted, inFormat).toFormat(outFormat)}</Text>
-        </LocationDetailsContainer>
-        <WeatherDescriptionContainer>
-          <Row style={{ marginBottom: 10 }}>
-            <WeatherIcon type={current.weather.main} size={40} />
-          </Row>
-          <Row>
-            <Text>{current.weather.main }</Text>
-          </Row>
-        </WeatherDescriptionContainer>
+        <Background>
+          <Degrees>{current.weather.temperature}°</Degrees>
+          <LocationDetailsContainer>
+            <Place>{location.name}, {location.country}</Place>
+            <Text inline>{DateTime.fromFormat(time.formatted, inFormat).toFormat(outFormat)}</Text>
+          </LocationDetailsContainer>
+          <WeatherDescriptionContainer>
+            <Row style={{ marginBottom: 10 }}>
+              <WeatherIcon type={current.weather.main} size={40} />
+            </Row>
+            <Row>
+              <Text>{current.weather.main }</Text>
+            </Row>
+          </WeatherDescriptionContainer>
+        </Background>
       </Container>
     ))
   )
